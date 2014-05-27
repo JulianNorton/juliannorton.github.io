fa18c45284d6c5d8da792d72f1d934e0459540aa
JulianNorton <julian@juliannorton.com>
2014-05-26 21:27:29 -0400

    no <script> in js file, duh

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-<script>
 $(function() {
 	$('a[href*=#]:not([href=#])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -14,4 +13,3 @@ $(function() {
 		}
 	});
 });
-</script>
