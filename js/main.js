a909d9f24f652c6033711bafd9cc447c147199a1
JulianNorton <julian@juliannorton.com>
2014-06-05 11:22:52 -0400

    no wrap function for entire js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
-$(function() {
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+$('a[href*=#]:not([href=#])').click(function() {
+	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top
-				}, 750, 'easeInOutCubic');
-				return false;
-			}
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+		if (target.length) {
+			$('html,body').animate({
+				scrollTop: target.offset().top
+			}, 750, 'easeInOutCubic');
+			return false;
 		}
-	});
+	}
+});
 
-	$(document).ready(function() {
+$(document).ready(function() {
 		$('.gallery').each(function() { // the containers for all your galleries
 			$(this).magnificPopup({
 			delegate: 'a', // the selector for gallery item
@@ -30,4 +29,3 @@ $(function() {
 		}); 
 
 	});
-})
